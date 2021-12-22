9073acd9ea1780bd97a7bd4ca73f6807b3d05402
leleoics <leonardocartografica@gmail.com>
2021-12-22 10:35:52 -0300

    Fixado tema padrão ligth para a aplicação

--- setup.sh
+++ setup.sh
@@ -11,3 +11,12 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[theme]\n\
+primaryColor=#F63366\n\
+backgroundColor=#FFFFFF\n\
+secondaryBackgroundColor=#F0F2F6\n\
+textColor=#262730\n\
+font=sans serif\n\
+" > ~/.streamlit/config.toml
