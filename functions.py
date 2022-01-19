4be71c92291837ff8ba8219de57eb01f67870e77
leleoics <leonardocartografica@gmail.com>
2022-01-19 12:02:45 -0300

    Alterado imports

--- functions.py
+++ functions.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import gspread
 from google.oauth2 import service_account
-import plotly_express as px
-import streamlit as st
 
 json_file = "./json/planejamento-constel123-1b8f07026a82.json"
 scopes = ["https://www.googleapis.com/auth/spreadsheets",
